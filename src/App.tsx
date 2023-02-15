6487ba4c34dec92f2be3d0134c7ea1396b15ca1f
DESKTOP-C43IAH4\Espen <espesele@stud.ntnu.no>
2023-02-15 10:49:24 +0100

    removed unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Header from './Components/Header/Header';
 import Admin from './pages/Admin';
 import Home from './pages/Home';
