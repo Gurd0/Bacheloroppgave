239edf8c75ec61af002d260e7fe295ebc76bfe70
Gard Furre <gardaf@stud.ntnu.no>
2023-02-24 10:34:45 +0100

    added empty components for page types

---
+++ src/pages/Course/components/CourseContent/CourseText.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const CourseText = () => {
+  return (
+    <div>CourseText</div>
+  )
+}
+
+
+export default CourseText
---
+++ src/pages/Course/components/CourseContent/CourseVideo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const CourseVideo = () => {
+  return (
+    <div>CourseText</div>
+  )
+}
+
+
+export default CourseVideo
