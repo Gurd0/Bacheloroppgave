2581654308abeb8c17e7052896b9627ca32377f9
Gard Furre <gardaf@stud.ntnu.no>
2023-02-24 10:33:45 +0100

    added queries and name chages to vars

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -1,84 +1,87 @@
-import React, { useState, useEffect, Children } from 'react';
+import React, { useState, useEffect } from 'react';
 import {db} from "../../firebase"
-import { collection, getDoc, doc, getDocs } from "firebase/firestore";
+import { collection, getDoc, doc, getDocs, DocumentReference, DocumentData } from "firebase/firestore";
 import { useParams } from 'react-router';
 import CourseTree from './components/CourseTree';
+import { useFullCourse, useCurrentPage } from './queries';
 
 import Grid from '@mui/material/Grid';
-
-type ChapterType = {
-  Pages: any,
-  ChapterName: string
-}
-
-type CourseType = {
-  Name: string; 
-  Chapters: any[];
-  id: string;
-}
+import CourseMobileStep from './components/CourseMobileStep';
+import { Box } from '@mui/system';
+import { useQuery } from 'react-query';
+import { FullCourse, PageType } from './context/context';
 
 const Index = () => {
-    const [courseObj, setCourseObj] = useState<CourseType>();
-    const [chapterObj, setChapterObj] = useState<ChapterType[]>([]);
-    //bør finn type 
     const { slug }: any = useParams();
+    
+    const [currentPageId, setCurrentPageId] = useState("")
+    const [currentPage, setCurrentPage] = useState<PageType>()
+    const [course, setCourse] = useState<FullCourse>()
+    
+    //queries hooks
+    const fullCourse = (useFullCourse(slug, "Courses"))
+    const page = (useCurrentPage(currentPageId, "Pages"))
+    
+    //TODO lægg tel animasjon når lastes
+    const [tree, setTree] = useState<RenderTree>({
+      id: "error",
+      name: "error",
+      children: [
+       
+     ]
+    })
+    useEffect(() => {
+      if(!page.isLoading){
+        setCurrentPage(page.data as PageType)
+        console.log(currentPage)
+      }
+    },[page])
+    useEffect(()=> {
+      if(fullCourse.isError) console.log("error")
+      if(fullCourse.isLoading) console.log("l")
+    
+      //console.log(c)
+      if(!fullCourse.isLoading){
+        setCourse(fullCourse.data as FullCourse)
+      }
+      
+    },[fullCourse])
 
-    useEffect(()=>{
-        const fetchPost = async () => {
-            //get course ref from id (slug)
-            const docRef = doc(db, "Courses", slug)
-            //get the data from the ref as courseTpe 
-            const course = (await getDoc(docRef)).data() as CourseType
-            const chapters = await Promise.all(
-              // get data for all chapters
-              course.Chapters.map(async (chapterRef: any) => {
-                const chapter = await getDoc(chapterRef);
-                const chapterData = chapter.data();
-                return chapterData as ChapterType
-              }
-              )
-            )
-            setChapterObj(chapters)
-            setCourseObj(course)
+    useEffect(() => {
+      if(course != undefined){
+        let tree: RenderTree = {
+          id: "courseObj.id",
+          name: course.Course,
+          children: [
+           
+         ]
         }
-        fetchPost();
-    }, [])
+        course.Chapters.map(chapter => {
+             const childrenPages: RenderTree[] = []
+             for (const [key, value] of Object.entries(chapter.Pages)) {
+               //make key type any to get objects
+               const keyAsAny = value as any
+               childrenPages.push({name: key, id: keyAsAny.id, page: true})
+             }
+             tree.children?.push({name: chapter.ChapterName, id: chapter.ChapterName, children: childrenPages})
+        })
+        //console.log(t)
+        setTree(tree)
+    }
+    },[course])
+    
+    const onPageClick = async (id: string) => {
+      setCurrentPageId(id)
+      console.log(page)
+      
+    }
+
   interface RenderTree {
     id: string;
     name: string;
     children?: RenderTree[];
     page?: boolean;
   }
-  function tree(): RenderTree {
-    if(courseObj){
-      let t: RenderTree = {
-       id: "courseObj.id",
-       name: courseObj.Name,
-       children: [
-        
-      ]
-     }
-     chapterObj.map(chapter => {
-        if(chapter){
-          const childrenPages: RenderTree[] = []
-          for (const [key, value] of Object.entries(chapter.Pages)) {
-            const keyAsAny = value as any
-            childrenPages.push({name: key, id: keyAsAny.id, page: true})
-          }
-          t.children?.push({name: chapter.ChapterName, id: chapter.ChapterName, children: childrenPages})
-        }
-     })
-     return t
-   }
-   else{
-    return {
-      id: '2',
-      name: '1',
-      children: [
-    ]
-    }
-   }
-  } 
   
   return (
     <>
@@ -87,17 +90,40 @@ const Index = () => {
       <div style={{
         border: '1px solid black',
       }}>
-      Hei  
+      <Box>
+      {currentPage?.Type === "text" &&
+        <h2>
+          Text
+        </h2>
+      }
+      {currentPage?.Type === "image" &&
+        <h2>
+          Image
+        </h2>
+      }
+      {currentPage?.Type === "video" &&
+        <h2>
+          Video
+        </h2>
+      }
+      {currentPage?.Type === "quiz" &&
+        <h2>
+          Quiz
+        </h2>
+      }
+      </Box>  
+      <CourseMobileStep />  
       </div>
     </Grid>
     <Grid item xs={4}>
-      <CourseTree {...tree()}/>
+      <CourseTree tree={tree} ClickHandler={onPageClick}/>
     </Grid>
-   
   </Grid>
-    
-   
+ 
     </>
   )
 }
 export default Index
+/* 
+ 
+*/
