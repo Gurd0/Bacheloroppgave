a0711cbcabc18e1e43b01555f4a9a440871df6c8
Gard <gardaf@stud.ntnu.no>
2023-03-21 12:22:32 +0100

    empty components for image, quiz and video

---
+++ src/pages/AdminCourse/components/courseEdit/imageEdit.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const ImageEdit = () => {
+  return (
+    <div>Image</div>
+  )
+}
+
+
+export default ImageEdit
---
+++ src/pages/AdminCourse/components/courseEdit/videoEdit.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const VideoEdit = () => {
+  return (
+    <div>CourseText</div>
+  )
+}
+
+
+export default VideoEdit
