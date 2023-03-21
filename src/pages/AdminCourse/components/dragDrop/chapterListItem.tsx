5ed3821df03fb4fd46e63029fa3c637e9d7c0e66
Gard <gardaf@stud.ntnu.no>
2023-03-21 12:20:38 +0100

    added drag and drop component and removed treeview

---
+++ src/pages/AdminCourse/components/dragDrop/chapterListItem.tsx
@@ -0,0 +1,102 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import React, { useMemo, useState } from "react";
+
+import styled, { css } from "styled-components";
+import { CourseType, PageType } from "../../../../context/context";
+import ListItem from "./PageItem";
+import { ChapterType } from "../../../Course/context/context";
+import { Box, Button, Popper, TextField } from "@mui/material";
+
+interface ToggleProps {
+    item : any
+    provided: any 
+    snapshot: any
+    addPage: (chapterId: string) => void
+    chapter: ChapterType
+    pages: PageType[]
+    changeChapterName: (name: string, id: string) => void
+    removePage: (chapterId: string, pageId: string) => void
+    setSelectedPage: any
+    selectedPage: any
+}
+const DragItem = styled.div`
+  padding: 10px;
+  border-radius: 6px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  background: white;
+  margin: 0 0 8px 0;
+  display: grid;
+  grid-gap: 20px;
+  flex-direction: column;
+`;
+
+ function ChapterListItem(Props: ToggleProps) {
+  const [page, setPage] = useState<PageType[]>(Props.pages);
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [textInput, setTextInput] = useState("")
+
+  const onDragEnd = (result: any) => {
+    const newpage = Array.from(page);
+    const [removed] = newpage.splice(result.source.index, 1);
+    newpage.splice(result.destination.index, 0, removed);
+    setPage(newpage);
+  };
+  
+  return (
+    <DragItem
+      ref={Props.provided.innerRef}
+      snapshot={Props.snapshot}
+      {...Props.provided.draggableProps}
+      {...Props.provided.dragHandleProps}
+    >
+        
+       <h2>{Props.chapter.ChapterName} <button onClick={() => Props.addPage(Props.chapter.id) }>Add Page</button>
+       <button onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+        setOpen(!open)
+        setAnchorEl(event.currentTarget);
+       }}>Change Name</button></h2>
+       <Popper id={"ChangeChapterName"} open={open} anchorEl={anchorEl}>
+            <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper'}}>
+            <TextField id="standard-basic" label="Page Name" variant="standard" onChange={(event) =>{
+                setTextInput(event.target.value);
+            }}/>
+            <Button onClick={() => {
+                setOpen(!open)
+                Props.changeChapterName(textInput, Props.chapter.id)
+            }}>Ok</Button>
+            </Box>
+        </Popper>
+       <DragDropContext onDragEnd={onDragEnd}>  
+        <Droppable droppableId="droppablePage" >  
+            {(provided) => (  
+                <div  
+                    {...provided.droppableProps}  
+                    ref={provided.innerRef}  
+                > 
+                    {page.map((page: PageType, index: number) => (  
+                        <Draggable key={page.id} draggableId={page.id} index={index} >  
+                            {(provided, snapshot) => (  
+                                <ListItem
+                                provided={provided}
+                                snapshot={snapshot}
+                                item={page}
+                                removeItem={Props.removePage}
+                                chapterId={Props.chapter.id}
+                                pageId={page.id}
+                                setSelectedPage={Props.setSelectedPage}
+                                selected={Props.selectedPage == page}
+                            />
+                            )}  
+                        </Draggable>  
+                    ))}  
+                    {provided.placeholder} 
+                </div>  
+            )}  
+        </Droppable> 
+    </DragDropContext>
+    </DragItem>
+  );
+};
+
+export default ChapterListItem;
--- src/pages/AdminCourse/index.tsx
+++ src/pages/AdminCourse/index.tsx
@@ -6,73 +6,140 @@ import Grid from '@mui/material/Grid';
 import { Box } from '@mui/system';
 import { useQuery } from 'react-query';
 
-import CourseTree from './components/courseTree';
-import { RenderTree } from '../../context/context';
+import { CourseType, FullCourse, RenderTree } from '../../context/context';
+import NewPage from './components/newPage';
+import ChapterDragDrop from './components/dragDrop/chapterDragDrop';
+import { ChapterType } from '../../context/context';
+import { Button, Popper, TextField } from '@mui/material';
+import { alignProperty } from '@mui/material/styles/cssUtils';
+import { PageType } from '../../context/context';
+import TextEdit from './components/courseEdit/textEdit';
+import ImageEdit from './components/courseEdit/imageEdit';
+import VideoEdit from './components/courseEdit/videoEdit';
+import QuizEdit from './components/courseEdit/quizEdit';
 
 function Index(){
   const { slug }: any = useParams();
-
-  const [tree, setTree] = useState<RenderTree>(
-    {
-        id: "daff",
-        name: "New Course",
-        course: true,
-        children: [
-            {
-            id: "1dsada",
-            name: "Add Chapter",
-            newChapter: true,
-            children: [
-
-            ]
-            }
-        ]
-      }
-  )
   
-  const onClick = (node: RenderTree) => {
-    if(node.newChapter){
-        console.log("chapter")
-        const t: RenderTree = tree
-        t.children?.push({id: "r" + t.children.length, name: "New Chapter", children: [{id: "newpage" + t.children.length, name: "Add Page", newPage: true, children: []}]})
-        setTree({...t})
+  const [chapters, setChapters] = useState<ChapterType[]>([])
+
+  const [selectedPage, setSelectedPage] = useState<PageType>()
+  const [selectedChapter, setSelectedChapter] = useState<ChapterType>()
+
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [textInput, setTextInput] = useState("")
+
+  const [course, setCourse] = useState<CourseType>({
+    Name: "Course",
+    Chapters: chapters,
+    id: "jkljlk" //TODO add random id / eller slug mæ random slug på ny 
+  })
+     
+  const addChapter = () =>{
+    const t: ChapterType =  {
+      Pages:  [],
+      ChapterName: "New Chapter",
+      id: Math.random().toString(36).substring(2,7)
     }
-    if(node.course){
-        console.log("course")
-    }
-    if(node.newPage){
-        console.log("page")
-        const t: RenderTree = tree
-        t.children?.forEach((i, chapterIndex) => {
-            if(i.children){
-                i.children.forEach((page, pageIndex) =>{
-                    if(page === node){
-                        if(t.children && t.children[chapterIndex]){
-                            t.children[chapterIndex].children.push({id: "dsa"+pageIndex, name: "New Page", children:[]})
-                        }
-                    }
-                    //i.children?.push({id: "plizz", name: "New Page"})
-                })
+    let chapterClone: ChapterType[] = chapters
+    chapterClone.push(t)
+    //console.log(chapterClone)
+    setChapters([...chapterClone])
+  }
+  const setPageType = (type: string) => {
+    if(selectedPage){
+      let chapterClone = chapters
+      let selectedPageClone = selectedPage
+      chapters.map((chapter, index) => {
+         chapter.Pages.map((page: PageType, indexPage: number) => {
+            if(page == selectedPage){
+              chapterClone[index].Pages[indexPage].type = type
+              setChapters([...chapterClone])
+              selectedPageClone.Type = type
+              setSelectedPage(selectedPageClone)
             }
-        
-        })
-        setTree({...t})
-        
-     //   console.log(tree)
+         })
+      })
     }
   }
+  const setPageValue = (value: any) => {
+    if(selectedPage){
+    //  console.log("setPageValue : " + value)
+    console.log(value)
+      let chapterClone = chapters
+      let selectedPageClone = selectedPage
+      chapters.map((chapter, index) => {
+         chapter.Pages.map((page: PageType, indexPage: number) => {
+            if(page == selectedPage){
+              chapterClone[index].Pages[indexPage].Value = value
+              setChapters([...chapterClone])
+             // selectedPageClone.Value = value
+            //  setSelectedPage(selectedPageClone)
+            }
+         })
+      })
+      //console.log(chapters)
+    }
+  }
+  
   return(
     <>
     <Grid container spacing={2}>
-    <Grid item xs={8}>
+    <Grid item xs={8} >
       <div style={{
         border: '1px solid black',
+        height: '100%'
       }}>
-     
+      <Box>
+       {selectedPage?.Type === "new" &&
+          <NewPage setPageType={setPageType}/>
+      }
+      {selectedPage?.Type === "Text" &&
+          <TextEdit setPageValue={setPageValue} pageValue={selectedPage.Value} selectedPage={selectedPage}/>
+      }
+      {selectedPage?.Type === "Image" &&
+          <ImageEdit />
+      }
+      {selectedPage?.Type === "Video" &&
+          <VideoEdit />
+      }
+      {selectedPage?.Type === "Quiz" &&
+          <QuizEdit />
+      }
+       </Box>
     </div>
     </Grid>
     <Grid item xs={4}>
-      <CourseTree tree={tree} ClickHandler={onClick}/>
+    <h2>{course.Name }
+    <button onClick={(event: React.MouseEvent<HTMLButtonElement>) =>{
+      setOpen(!open)
+      setAnchorEl(event.currentTarget);
+    }}>
+      Change Name
+    </button>
+     <button onClick = {() =>{
+          addChapter()
+        }} >Add Chapter</button></h2>
+
+    <Popper id={"ChangeChapterName"} open={open} anchorEl={anchorEl}>
+      <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper'}}>
+      <TextField id="standard-basic" label="Page Name" variant="standard" onChange={(event) =>{
+        setTextInput(event.target.value);
+      }}/>
+      <Button onClick={() => {
+        const corseClone: CourseType = course
+        corseClone.Name = textInput
+        setCourse(corseClone)
+        setOpen(!open)
+      }}>Ok</Button>
+      </Box>
+    </Popper>
+
+    {chapters ?
+      <ChapterDragDrop chapters={chapters} setChapters={setChapters} setSelectedPage={setSelectedPage} selectedPage={selectedPage}/>
+    : <h1>hmm</h1>
+    }
     </Grid>
   </Grid>
     </>
