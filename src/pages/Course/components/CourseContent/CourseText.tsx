244084058c93d0dff584a07a56af01d552ea20eb
Gard Furre <gardaf@stud.ntnu.no>
2023-03-28 14:00:11 +0200

    added component for viewing pages of type text

--- src/pages/Course/components/CourseContent/CourseText.tsx
+++ src/pages/Course/components/CourseContent/CourseText.tsx
@@ -1,9 +1,35 @@
+import { convertFromRaw, Editor, EditorState } from 'draft-js'
 import React from 'react'
+import { PageType } from '../../../../context/context'
+import { stateToHTML } from 'draft-js-export-html';
 
 
-const CourseText = () => {
+interface PropsType {
+  currentPage: PageType
+}
+const CourseText = (props: PropsType) => {
+  const contentState = convertFromRaw(props.currentPage.Value);
+  const editorState = EditorState.createWithContent(contentState);
+  let options = {
+    inlineStyles: {
+      // Override default element (`strong`).
+      BOLD: {element: 'b'},
+      ITALIC: {
+        // Add custom attributes. You can also use React-style `className`.
+        attributes: {class: 'foo'},
+        // Use camel-case. Units (`px`) will be added where necessary.
+        style: {fontSize: 12}
+      },
+      // Use a custom inline style. Default element is `span`.
+      RED: {style: {color: '#900'}},
+    },
+  };
+  const html = stateToHTML(editorState.getCurrentContent(), options)
+  let _html = html.replace("", '');
   return (
-    <div>CourseText</div>
+    <div dangerouslySetInnerHTML={{__html : _html}}>
+    </div>
+  
   )
 }
 
