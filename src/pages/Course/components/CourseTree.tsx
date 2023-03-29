6e81c436677bee31d0e2092a058b0d56466ac8a9
Gard <gardaf@stud.ntnu.no>
2023-03-29 15:23:14 +0200

    made course tree view controlled

--- src/pages/Course/components/CourseTree.tsx
+++ src/pages/Course/components/CourseTree.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import TreeItem from '@mui/lab/TreeItem';
+import TreeItem, { treeItemClasses } from '@mui/lab/TreeItem';
 import RenderTree from '../context/RenderrTree';
+import { styled } from '@mui/material';
+import { Style } from 'util';
+import { SxProps } from '@mui/material/styles';
 
 //add state for selected row clicked. 
 
@@ -12,33 +15,68 @@ interface ToggleProps {
   ClickHandler: (id: string) => void
   tree: RenderTree
 }
+const StyledTreeView ={
+//  marginLeft: 0,
+}
+
+
+const StyledTreeItem = {
+ // display: "none",
+}
 
 export default function CourseTree(Props: ToggleProps) {
-  const renderTree = (nodes: RenderTree) => (
-    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name} onClick={() => {
-      if(nodes.page){
-        Props.ClickHandler(nodes.id)
-      }
-    }}
+  const [expanded, setExpanded] = React.useState<string[]>([]);
+  const [selected, setSelected] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    const t = selected.join("")
+    if(selected){
+      Props.ClickHandler(t)
+    } 
+    console.log(t)
+  },[selected])
+
+  const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+    setExpanded(nodeIds);
+  };
+
+  const handleSelect = (event: React.SyntheticEvent, nodeIds: string[]) => {
+    setSelected([...nodeIds]);
+  };
+
+  const renderTree = (nodes: RenderTree, style?: SxProps) => (
+    <TreeItem 
+    sx={ style }
+    key={nodes.id} 
+    nodeId={nodes.id} 
+    label={nodes.name} 
     >
       {Array.isArray(nodes.children)
-        ? nodes.children.map((node) => renderTree(node))
+        ? nodes.children.map((node) => renderTree(node, StyledTreeItem))
         : null}
         
     </TreeItem>
   );
   //console.log(course)
-
+  
 
     return (
       <TreeView
-        aria-label="rich object"
+        aria-label="controlled"
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpanded={['root']}
         defaultExpandIcon={<ChevronRightIcon />}
-        sx={{ height: 500, flexGrow: 1, maxWidth: "100%"}}
+        expanded={expanded}
+        selected={selected}
+        onNodeToggle={handleToggle}
+        onNodeSelect={handleSelect}
+        //expanded={expanded}
+        sx={{ 
+          height: 500,
+          flexGrow: 1,
+          maxWidth: "100%",}}
       >
-        {renderTree(Props.tree)}
+        {renderTree(Props.tree, StyledTreeView)}
       </TreeView>
     );
 }
