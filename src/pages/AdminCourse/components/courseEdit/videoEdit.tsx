d2b5c1fe7d0f9b2c04d8abebb6b3fa08836806aa
Gard Furre <gardaf@stud.ntnu.no>
2023-03-31 15:00:35 +0200

    added video and image to course create and view

--- src/pages/AdminCourse/components/courseEdit/videoEdit.tsx
+++ src/pages/AdminCourse/components/courseEdit/videoEdit.tsx
@@ -1,9 +1,45 @@
+import TextField from '@mui/material/TextField'
 import React from 'react'
+import { PageType } from '../../../../context/context'
+
+interface ToggleProps {
+  setPageValue: (value: any) => void
+  pageValue: any
+  selectedPage: PageType 
+}
 
 
-const VideoEdit = () => {
+const VideoEdit = (props: ToggleProps) => {
+  const [textField, setTextField] = React.useState<string>()
+  const [wrongUrl, setWrongUrl] = React.useState<boolean>(false)
+
+  const handleTextInputChange = (event: { target: { value: React.SetStateAction<string | undefined> } }) => {
+    setTextField(event.target.value);
+  };
+  React.useEffect(() => {
+    console.log(props.pageValue)
+    if(typeof props.pageValue === "string"){
+      setTextField(props.pageValue)
+    }
+  },[props.pageValue])
+  React.useEffect(() => {
+    const pattern = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/.+$/
+    if(textField)
+    if(pattern.test(textField)){
+      setWrongUrl(true)
+      props.setPageValue(textField)
+    }else{
+      setWrongUrl(false)
+    }
+  },[textField])
   return (
-    <div>CourseText</div>
+     <div style={{
+      textAlign: 'center',
+     }}>
+        <h2>Video Url</h2>
+        <TextField value={textField} onChange={handleTextInputChange} />
+        <h2>{wrongUrl.toString()}</h2>
+     </div>
   )
 }
 
