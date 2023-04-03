ed605557d72c949efeb4278c9f1c958a5aac0edf
Gard <gardaf@stud.ntnu.no>
2023-04-03 16:24:58 +0200

    added icon to course tree based on type

--- src/pages/Course/components/CourseTree.tsx
+++ src/pages/Course/components/CourseTree.tsx
@@ -5,9 +5,16 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem, { treeItemClasses } from '@mui/lab/TreeItem';
 import RenderTree from '../context/RenderrTree';
-import { styled } from '@mui/material';
+import { Card, Paper, styled, SvgIcon, SvgIconTypeMap } from '@mui/material';
 import { Style } from 'util';
 import { SxProps } from '@mui/material/styles';
+import { width } from '@mui/system';
+
+//icons
+import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
+import OndemandVideoOutlinedIcon from '@mui/icons-material/OndemandVideoOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 //add state for selected row clicked. 
 
@@ -22,12 +29,16 @@ const StyledTreeView ={
 
 
 const StyledTreeItem = {
- // display: "none",
+  //display: "none",
+   //height: "4em",
+   //padding: "10px"
 }
 
 export default function CourseTree(Props: ToggleProps) {
   const [expanded, setExpanded] = React.useState<string[]>([]);
   const [selected, setSelected] = React.useState<string[]>(Props.selectedNode);
+
+ 
 
   React.useEffect(() => {
     const t = selected.join("")
@@ -51,18 +62,61 @@ export default function CourseTree(Props: ToggleProps) {
   };
 
   const renderTree = (nodes: RenderTree, style?: SxProps) => (
+    
     <TreeItem 
     sx={ style }
     key={nodes.id} 
     nodeId={nodes.id} 
     label={nodes.name}
     >
+      
       {Array.isArray(nodes.children)
-        ? nodes.children.map((node) => renderTree(node, StyledTreeItem))
+        ? nodes.children.map((node) => renderTreeChild(node, StyledTreeItem))
         : null}
-        
+      
     </TreeItem>
+    
   );
+  const renderTreeChild = (nodes: RenderTree, style?: SxProps) => {
+    let logo;
+    if(nodes.type == "Text"){
+      logo = TextSnippetOutlinedIcon
+    }
+    if(nodes.type == "Image"){
+      logo = ImageOutlinedIcon
+    }
+    if(nodes.type == "Video"){
+      logo = OndemandVideoOutlinedIcon
+    }
+    else{
+      logo = TextSnippetOutlinedIcon
+    }
+    console.log("Type ! : " + nodes.type)
+    return(
+    <TreeItem 
+    sx={ style }
+    key={nodes.id} 
+    nodeId={nodes.id} 
+    label={<div>
+      <p style={{display: 'flex', justifyContent: 'space-between'}}>
+      {nodes.name}
+      
+      {nodes.type == "Text" &&
+        <TextSnippetOutlinedIcon/>
+      }
+      {nodes.type == "Image" &&
+        <ImageOutlinedIcon/>
+      }
+      {nodes.type == "Video" &&
+        <OndemandVideoOutlinedIcon/>
+      }
+      
+      </p>
+    </div>}
+    >
+    </TreeItem>
+    
+  )}
   //console.log(course)
   
 
@@ -77,9 +131,9 @@ export default function CourseTree(Props: ToggleProps) {
         onNodeToggle={handleToggle}
         onNodeSelect={handleSelect}
         sx={{ 
-        
           flexGrow: 1,
-          maxWidth: "90%",}}
+          maxWidth: "90%",
+        }}
       >
         {renderTree(Props.tree, StyledTreeView)}
       </TreeView>
