62585041d720b3de963a053ec10a7f9a8444988b
Gard <gardaf@stud.ntnu.no>
2023-04-05 14:58:11 +0200

    admin course list by topic

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -17,6 +17,9 @@ import { useFullCourse, useGetCollection } from "../../hooks/queries";
 export default function Home() {
   const [courses, setCourses] = useState<CourseType[]>([]);
   const [draftCourses, setDraftCourses] = useState<CourseType[]>([]);
+
+  const [courseTopicMap, setCoursesTopicMap] = useState<Map<string, CourseType[]>>(new Map())
+
   const fullCourse = useGetCollection("Courses", false);
   const draftCoursesHook = useGetCollection("Courses", true)
 
@@ -27,6 +30,22 @@ export default function Home() {
   }, [fullCourse]);
 
   useEffect(() => {
+    let myMap = new Map<string, CourseType[]>();
+      courses.map((c) =>{
+        if(c.Topic && myMap.has(c.Topic)){
+          let temp: CourseType[] = myMap.get(c.Topic) as CourseType[]
+          temp.push(c)
+          myMap.set(c.Topic, temp)
+          setCoursesTopicMap(courseTopicMap.set(c.Topic, temp))
+        }else if(c.Topic){
+          myMap.set(c.Topic, [c])
+          setCoursesTopicMap(courseTopicMap.set(c.Topic, [c]))
+        }
+      })
+      setCoursesTopicMap(myMap)
+  },[courses])
+
+  useEffect(() => {
     if (!draftCoursesHook.isLoading) {
       setDraftCourses(draftCoursesHook.data as CourseType[]);
     }
@@ -34,38 +53,50 @@ export default function Home() {
 
   return (
     <Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <div>
-          <h1 style={{ textAlign: "center" }}>Gitar</h1>
-        </div>
+      
         {fullCourse.isLoading ? (
           <CircularProgress />
         ) : (
-          <Grid
-            sx={{
-              padding: "1em",
-            }}
-            container
-            spacing={3}
-          >
-            {courses.map((courses) => (
-              <Grid item xs={12} sm={6} md={4}>
-                {/* key={courses.id} */}
-                <CardActionArea href={"/admin/edit/" + courses.id}>
+          <>
+          {[...courseTopicMap.keys()].map((k) => {
+            return(
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+             
+            <div>
+              <h1 style={{ textAlign: "center" }}>{k}</h1>
+            </div>
+
+            <Grid
+              sx={{
+                padding: "1em",
+              }}
+              container
+              spacing={3}
+            >
+            {courseTopicMap.get(k)?.map((course) => {
+              return (
+                <Grid item xs={12} sm={6} md={4}>
+
+                <CardActionArea href={"/course/" + course.id}>
                   <Card>
-                    <CardHeader title={courses.Name} />
+                    <CardHeader title={course.Name} />
                     <CardMedia component="img" height="200" image="#" alt="#" />
                   </Card>
                 </CardActionArea>
-              </Grid>
-            ))}
-          </Grid>
+              </Grid> 
+              )
+            })}
+            </Grid>
+            </Box>
+            )
+           })} 
+          </>
         )}
          <div>
           <h1 style={{ textAlign: "center" }}>Draft</h1>
@@ -94,6 +125,5 @@ export default function Home() {
           </Grid>
         )}
       </Box>
-    </Box>
   );
 }
