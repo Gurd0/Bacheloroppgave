506d07a2f71349a60a52ab932281963048f21112
DESKTOP-C43IAH4\Espen <espesele@stud.ntnu.no>
2023-04-11 18:08:23 +0200

    Styling applied locked-completed-available courses

--- src/pages/Home/components/DisabledCard.tsx
+++ src/pages/Home/components/DisabledCard.tsx
@@ -1,25 +1,52 @@
-import { CardActionArea, CardHeader, CardMedia, Card} from "@mui/material"
-import React from "react"
-import { CourseType } from "../../../context/context"
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardHeader,
+  CardMedia,
+} from "@mui/material";
+import React from "react";
+import { CourseType } from "../../../context/context";
 
-import LockIcon from '@mui/icons-material/Lock';
+import LockIcon from "@mui/icons-material/Lock";
 interface PropsType {
-    course: CourseType
+  course: CourseType;
 }
 
 const DisabledCard = (props: PropsType) => {
-    return (
-        <>
-        <CardActionArea href={"/course/" + props.course.id} disabled={true}>
-            <Card>
-                <CardHeader title={props.course.Name } />
-                <CardMedia component="img" height="200" image="#" alt="#" />
-            </Card>
-        <LockIcon />       
+  return (
+    <>
+      <Box
+        sx={{
+          backdropFilter: "blur(20px)",
+        }}
+      >
+        <CardActionArea
+          sx={{
+            background: " rgba(193, 185, 185, 0.08)",
+            borderRadius: "16px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.15)",
+            border: "1px solid rgba(193, 185, 185, 0.52)",
+          }}
+          href={"/course/" + props.course.id}
+          disabled={true}
+        >
+          <Card
+            sx={{
+              backgroundColor: "lightgray",
+              opacity: "0.7",
+              borderRadius: "16px",
+              boxShadow: "0 4px 30px rgba(0, 0, 0, 0.15)",
+              border: "1px solid rgba(193, 185, 185, 0.52)",
+            }}
+          >
+            <CardHeader title={props.course.Name} />
+            <CardMedia component="img" height="200" image="#" alt="#" />
+          </Card>
         </CardActionArea>
-      </>
-    )
-  }
-  
-  
-  export default DisabledCard
+      </Box>
+    </>
+  );
+};
+
+export default DisabledCard;
