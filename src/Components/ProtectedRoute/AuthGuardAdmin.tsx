1204b20e8b338282552434039dba9e7052bce667
Gard Furre <gardaf@stud.ntnu.no>
2023-05-09 18:05:03 +0200

    AuthGuardAdmin for admin

---
+++ src/Components/ProtectedRoute/AuthGuardAdmin.tsx
@@ -0,0 +1,17 @@
+import { LinearProgress } from "@mui/material";
+import { Fragment, ReactElement, ReactNode, useContext } from "react";
+import { Navigate, NavigationType, Route } from "react-router-dom";
+import { AuthContext, UserAuth } from "../../context/auth-context";
+
+export const AuthGuardAdmin = ({ children }: { children: ReactNode }) => {
+  const { user, isLoading, admin} = useContext(AuthContext);
+    console.log(admin)
+  if (isLoading) {
+    return <LinearProgress />;
+  } else if (!user) {
+    return <Navigate to="/login" />;
+  } else if (!admin && user){
+    return <Navigate to="/error" state={"Bruker har ikke admin tilgang"}/>
+  }
+  return <>{children}</>;
+};
