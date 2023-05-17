e838db1542108cad388aaf6992485f39e92ab040
Gard Furre <gardaf@stud.ntnu.no>
2023-05-17 17:14:43 +0200

    removed unused function

--- src/pages/AdminCourse/components/courseEdit/imageUploader.tsx
+++ src/pages/AdminCourse/components/courseEdit/imageUploader.tsx
@@ -28,34 +28,6 @@ const ImageUploader = (props: ToggleProps) => {
   const [success, setSuccess] = useState<boolean>(false);
 
 
-  function CircularProgressWithLabel(
-    props: CircularProgressProps & { value: number }
-  ) {
-    // if (props.value === 100) return <p>No background activity</p>;
-    return (
-      <Box sx={{ position: "relative", display: "inline-flex" }}>
-        <CircularProgress variant="determinate" {...props} />
-        <Box
-          sx={{
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: "absolute",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Typography
-            variant="caption"
-            component="div"
-            color="text.secondary"
-          >{`${Math.round(props.value)}%`}</Typography>
-        </Box>
-      </Box>
-    );
-  }
 
   // 'file' comes from the Blob or File API
   React.useEffect(() => {
