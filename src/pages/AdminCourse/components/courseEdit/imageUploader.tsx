7e1ca12d6a18dc08b42a75e2a21d2d83708b3bdf
DESKTOP-C43IAH4\Espen <espesele@stud.ntnu.no>
2023-05-17 14:44:06 +0200

    removed unnecessary console.logs

--- src/pages/AdminCourse/components/courseEdit/imageUploader.tsx
+++ src/pages/AdminCourse/components/courseEdit/imageUploader.tsx
@@ -83,22 +83,17 @@ const ImageUploader = (props: ToggleProps) => {
 
   const upload = () => {
     if (file && reference) {
+      //Code inspired from https://firebase.google.com/docs/storage/web/upload-files
       const uploadTask = uploadBytesResumable(reference, file);
       uploadTask.on(
         "state_changed",
         (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-          console.log("Upload is " + progress + "% done");
-
           switch (snapshot.state) {
             case "paused":
-              console.log("Upload is paused");
               setUploading(false);
               break;
             case "running":
-              console.log("Upload is running");
               setUploading(true);
               break;
           }
