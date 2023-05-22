e79c905d5a37449dec3e4b346ecc91aec8e73542
DESKTOP-C43IAH4\Espen <espesele@stud.ntnu.no>
2023-05-22 02:09:46 +0200

    Citation of inspiration

--- src/pages/Login/SignUp/Form.tsx
+++ src/pages/Login/SignUp/Form.tsx
@@ -1,5 +1,9 @@
 import { Box, Button, TextField } from "@mui/material";
-import { createUserWithEmailAndPassword, updateProfile, UserCredential } from "firebase/auth";
+import {
+  UserCredential,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
@@ -29,19 +33,19 @@ const sxStyleTextField = {
 const sxStyleButton = {
   margin: 2,
 };
-
+// This Form-component is somewhat inspired from https://levelup.gitconnected.com/create-a-signup-page-with-react-and-material-ui-9b203d18cf3f
 const Form = (props: props) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [feedBack, setFeedBack] = useState("none")
+  const [feedBack, setFeedBack] = useState("none");
 
   //TODO: Need to update profile with given firstname and lastname
   const handleSubmit = async (e: any) => {
-    e.preventDefault()
-    
+    e.preventDefault();
+
     await signUpUser(email, password)
       .then((userCredentials: UserCredential) => {
         const name = firstName + " " + lastName;
@@ -49,10 +53,11 @@ const Form = (props: props) => {
           displayName: name,
           email: userCredentials.user.email,
         });
-      }).catch((err) => {
-        const error = err.toString().split(/[()]/)
-        const userError = error[1].split("/")
-        setFeedBack(userError[1])
+      })
+      .catch((err) => {
+        const error = err.toString().split(/[()]/);
+        const userError = error[1].split("/");
+        setFeedBack(userError[1]);
       })
       .then((userCredentials) => {
         if (auth.currentUser) {
@@ -60,7 +65,6 @@ const Form = (props: props) => {
             displayName: firstName + " " + lastName,
           });
           props.handleClose();
-          
         } else {
           console.log(error);
         }
@@ -68,69 +72,72 @@ const Form = (props: props) => {
       .catch((error) => {
         setError(error.message);
       });
-   
   };
 
   return (
     //onSubmit={handleSubmit} TODO: On submit with button type="submit" results in error in regards to firebase.
     <>
-    {(feedBack != "none" && feedBack != "Success") && 
-          <FeedBackError feedBack={feedBack} open={true} setFeedBack={setFeedBack}/>
-      }
-    <Box component="form" sx={formStyle} noValidate >
-      <TextField
-        sx={sxStyleTextField}
-        label="Fornavn"
-        variant="filled"
-        required
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <TextField
-        sx={sxStyleTextField}
-        label="Etternavn"
-        variant="filled"
-        required
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <TextField
-        sx={sxStyleTextField}
-        label="Email"
-        variant="filled"
-        type="email"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <TextField
-        sx={sxStyleTextField} 
-        label="Passord"
-        variant="filled"
-        type="password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <div>
-        <Button
-          variant="contained"
-          onClick={props.handleClose}
-          sx={sxStyleButton}
-        >
-          Avbryt
-        </Button>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          sx={sxStyleButton}
-          onClick={handleSubmit}
-        >
-          Fullfør
-        </Button>
-      </div>
-    </Box>
+      {feedBack != "none" && feedBack != "Success" && (
+        <FeedBackError
+          feedBack={feedBack}
+          open={true}
+          setFeedBack={setFeedBack}
+        />
+      )}
+      <Box component="form" sx={formStyle} noValidate>
+        <TextField
+          sx={sxStyleTextField}
+          label="Fornavn"
+          variant="filled"
+          required
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <TextField
+          sx={sxStyleTextField}
+          label="Etternavn"
+          variant="filled"
+          required
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <TextField
+          sx={sxStyleTextField}
+          label="Email"
+          variant="filled"
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          sx={sxStyleTextField}
+          label="Passord"
+          variant="filled"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div>
+          <Button
+            variant="contained"
+            onClick={props.handleClose}
+            sx={sxStyleButton}
+          >
+            Avbryt
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={sxStyleButton}
+            onClick={handleSubmit}
+          >
+            Fullfør
+          </Button>
+        </div>
+      </Box>
     </>
   );
 };
